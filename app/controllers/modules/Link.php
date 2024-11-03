fb9cccd068612ebfecf7b32e3464f47c21ccb535
Usbac <castillo.alex97@outlook.com>
2024-11-03 20:09:43 +0100

    Update namespace

---
+++ app/controllers/modules/Link.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Aurora\App\Modules;
+
+final class Link extends \Aurora\App\ModuleBase
+{
+    public const DEFAULT_ORDER = 'title';
+    public const DEFAULT_SORT = 'asc';
+
+    protected string $table = 'links';
+    protected array $orders = [
+        'title' => 'links.title',
+        'url' => 'links.url',
+        'order' => 'links.`order`',
+        'status' => 'links.status',
+    ];
+
+    /**
+     * Returns the links that must be shown in the header
+     * @return array the links
+     */
+    public function getHeaderLinks(): array
+    {
+        return $this->getPage(null, null, 'status', 'order');
+    }
+
+    /**
+     * Adds a new link
+     * @param array $data the link data
+     * @return string|bool the id of the new link on success, false otherwise
+     */
+    public function add(array $data): string|bool
+    {
+        return $this->db->insert($this->table, $this->getBaseData($data));
+    }
+
+    /**
+     * Updates an existing link
+     * @param int $id the link id
+     * @param array $data the new data
+     * @return bool true on success, false otherwise
+     */
+    public function save(int $id, array $data): bool
+    {
+        return $this->db->update($this->table, $this->getBaseData($data), $id) ? $id : false;
+    }
+
+    /**
+     * Returns an array with all the link fields that contain an error
+     * @param array $data the link fields
+     * @param [mixed] $id the link id
+     * @return array the array with the link fields that contain an error
+     */
+    public function checkFields(array $data, $id = null): array
+    {
+        $errors = [];
+
+        if (empty($data['title'])) {
+            $errors['title'] = $this->language->get('invalid_value');
+        }
+
+        if (!\Aurora\App\Permission::can('edit_links')) {
+            http_response_code(403);
+            $errors[0] = $this->language->get('no_permission');
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Returns the query conditions to obtain links based on the given filters
+     * @param array $filters the filters
+     * @return string the query conditions
+     */
+    public function getCondition(array $filters): string
+    {
+        $where = [];
+
+        if (isset($filters['status']) && $filters['status'] !== '') {
+            $where[] = 'links.status = ' . ((int) $filters['status']);
+        }
+
+        if (!empty($filters['search'])) {
+            $search = $this->db->escape($filters['search']);
+            $where[] = "(links.title LIKE '%$search%' OR links.url LIKE '%$search%')";
+        }
+
+        return implode(' AND ', $where);
+    }
+
+    /**
+     * Returns the right data to add or save a link
+     * @param array $data the link data
+     * @return array the right data
+     */
+    private function getBaseData(array $data): array
+    {
+        return [
+            'title' => $data['title'],
+            'url' => $data['url'],
+            'order' => $data['order'],
+            'status' => $data['status'],
+        ];
+    }
+}
---
+++ app/controllers/modules/Tag.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Aurora\App\Modules;
+
+final class Tag extends \Aurora\App\ModuleBase
+{
+    public const DEFAULT_ORDER = 'name';
+    public const DEFAULT_SORT = 'asc';
+
+    protected string $select = 'tags.*, COUNT(post_id) AS posts';
+    protected string $table = 'tags';
+    protected string $join = 'LEFT JOIN posts_to_tags p2t ON p2t.tag_id = tags.id AND p2t.post_id IN (SELECT posts.id FROM posts)';
+    protected string $group_by = 'tags.id';
+    protected array $relations = [ 'posts_to_tags' => 'tag_id' ];
+    protected array $orders = [
+        'name' => 'tags.name',
+        'posts' => 'COUNT(post_id)',
+    ];
+
+    /**
+     * Adds a new tag
+     * @param array $data the tag data
+     * @return string|bool the id of the new tag on success, false otherwise
+     */
+    public function add(array $data): string|bool
+    {
+        return $this->db->insert($this->table, $this->getBaseData($data));
+    }
+
+    /**
+     * Updates an existing tag
+     * @param int $id the tag id
+     * @param array $data the new data
+     * @return bool true on success, false otherwise
+     */
+    public function save(int $id, array $data): bool
+    {
+        return $this->db->update($this->table, $this->getBaseData($data), $id) ? $id : false;
+    }
+
+    /**
+     * Returns an array with all the tag fields that contain an error
+     * @param array $data the tag fields
+     * @param [mixed] $id the tag id
+     * @return array the array with the tag fields that contain an error
+     */
+    public function checkFields(array $data, $id = null): array
+    {
+        $errors = [];
+
+        if (empty($data['name'])) {
+            $errors['name'] = $this->language->get('invalid_value');
+        }
+
+        if (!empty($data['slug']) &&
+            !empty($this->get([ 'slug' => $data['slug'], '!id' => $id ]))) {
+            $errors['slug'] = $this->language->get('repeated_slug');
+        }
+
+        if (empty($data['slug']) || !\Aurora\Core\Helper::isSlugValid($data['slug'])) {
+            $errors['slug'] = $this->language->get('invalid_slug');
+        }
+
+        if (!\Aurora\App\Permission::can('edit_tags')) {
+            http_response_code(403);
+            $errors[0] = $this->language->get('no_permission');
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Returns the query conditions to obtain tags based on the given filters
+     * @param array $filters the filters
+     * @return string the query conditions
+     */
+    public function getCondition(array $filters): string
+    {
+        $where = [];
+
+        if (!empty($filters['search'])) {
+            $search = $this->db->escape($filters['search']);
+            $where[] = "(tags.name LIKE '%$search%' OR tags.slug LIKE '%$search%')";
+        }
+
+        return implode(' AND ', $where);
+    }
+
+    /**
+     * Returns the right data to add or save a tag
+     * @param array $data the tag data
+     * @return array the right data
+     */
+    private function getBaseData(array $data): array
+    {
+        return [
+            'name' => $data['name'],
+            'slug' => $data['slug'],
+            'description' => $data['description'],
+            'meta_title' => $data['meta_title'],
+            'meta_description' => $data['meta_description'],
+        ];
+    }
+}
