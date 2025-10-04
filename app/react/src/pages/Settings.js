06dba4e360c9031ed4d64b19646a70091627b3c8
Usbac <castillo.alex97@outlook.com>
2025-10-04 23:52:59 +0200

    Dev of new settings page

--- app/react/src/pages/Settings.js
+++ app/react/src/pages/Settings.js
@@ -1,13 +1,80 @@
 import React, { useEffect, useState } from 'react';
-import { LoadingPage, MenuButton } from '../utils/utils';
+import { getContentUrl, ImageDialog, Input, LoadingPage, MenuButton, Switch } from '../utils/utils';
 import { IconCode, IconDatabase, IconNote, IconServer, IconSettings, IconSync, IconTerminal } from '../utils/icons';
 import { useLocation, useOutletContext } from 'react-router-dom';
+
+const General = ({ data, setData }) => {
+    const [ open_image_dialog, setOpenImageDialog ] = useState(false);
+
+    return <div class="grid">
+        <div class="card v-spacing">
+            <div class="input-group">
+                <label>Logo</label>
+                <img src={data.logo ? getContentUrl(data.logo) : '/public/assets/no-image.svg'} class={`logo pointer ${!data.logo ? 'empty-img' : ''}`} alt="logo" onClick={() => setOpenImageDialog(true)}/>
+                {open_image_dialog && <ImageDialog onSave={path => setData({ ...data, logo: path })} onClose={() => setOpenImageDialog(false)}/>}
+            </div>
+            <div class="input-group">
+                <label htmlFor="title">Title</label>
+                <Input id="title" type="text" value={data.title} onChange={e => setData({ ...data, title: e.target.value })} charCount={true}/>
+            </div>
+            <div class="input-group-container">
+                <div class="input-group">
+                    <label htmlFor="blog-url">Blog URL</label>
+                    <Input id="blog-url" type="text" placeholder="/blog" value={data.blog_url} onChange={e => setData({ ...data, blog_url: e.target.value })}/>
+                </div>
+                <div class="input-group">
+                    <label htmlFor="rss">RSS feed URL</label>
+                    <Input id="rss" type="text" placeholder="/rss" value={data.rss} onChange={e => setData({ ...data, rss: e.target.value })}/>
+                </div>
+            </div>
+            <div class="input-group-container">
+                <div class="input-group">
+                    <label>Theme</label>
+                    <select name="theme">
+                        {Object.keys(data.meta.themes).map(theme => <option value={theme} selected={data.theme == data.meta.themes[theme]}>{data.meta.themes[theme]}</option>)}
+                    </select>
+                </div>
+                <div class="input-group">
+                    <label htmlFor="per_page">Items per page</label>
+                    <input id="per_page" name="per_page" type="number" placeholder="20" min="1" value={data.per_page} onChange={e => setData({ ...data, per_page: e.target.value })}/>
+                </div>
+            </div>
+            <div class="input-group-container">
+                <div class="input-group">
+                    <label>System language</label>
+                    <span class="description">System language</span>
+                    <select name="language">
+                        {data.meta.languages.map(lang => <option value={lang} selected={data.language == lang}>{lang}</option>)}
+                    </select>
+                </div>
+                <div class="input-group">
+                    <label htmlFor="date_format">Date format</label>
+                    <span class="description">Must follow a valid <a href="https://unicode-org.github.io/icu/userguide/format_parse/datetime/#datetime-format-syntax" target="_blank">ICU date</a> format</span>
+                    <input id="date_format" name="date_format" type="text" placeholder="MMM d, Y" value={data.date_format} onChange={e => setData({ ...data, date_format: e.target.value })}/>
+                </div>
+            </div>
+            <div class="input-group-container">
+                <div class="input-group">
+                    <label>Timezone</label>
+                    <select name="timezone">
+                        {data.meta.timezones.map(tz => <option value={tz} selected={data.timezone == tz}>{tz.replace('_', ' ')}</option>)}
+                    </select>
+                </div>
+                <div class="input-group">
+                    <label>Maintenance mode</label>
+                    <Switch checked={data.maintenance} onChange={e => setData({ ...data, maintenance: e.target.checked })}/>
+                </div>
+            </div>
+        </div>
+    </div>;
+};
 
 export default function Settings() {
     const version = document.querySelector('meta[name="version"]')?.content;
     const location = useLocation();
     const [ hash, setHash ] = useState(location.hash);
     const { user, settings } = useOutletContext();
+    const [ data, setData ] = useState(undefined);
 
     useEffect(() => {
         const onHashChange = () => setHash(window.location.hash);
@@ -15,11 +82,20 @@ export default function Settings() {
         return () => window.removeEventListener('hashchange', onHashChange);
     }, []);
 
-    if (!settings) {
+    useEffect(() => {
+        setData(settings);
+    }, [ settings ]);
+
+    const save = e => {
+        e.preventDefault();
+        console.log(data);
+    };
+
+    if (!data) {
         return <LoadingPage/>;
     }
 
-    return (<form id="settings-form" class="content">
+    return (<form id="settings-form" class="content" onSubmit={save}>
         <div>
             <div class="page-title">
                 <MenuButton/>
@@ -33,17 +109,20 @@ export default function Settings() {
             <div class="grid">
                 <div>
                     <div class="tabs">
-                        <a href="#general"><IconSettings/> General</a>
-                        <a href="#meta"><IconNote/> Meta</a>
-                        <a href="#data"><IconDatabase/> Data</a>
-                        <a href="#advanced"><IconTerminal/> Advanced</a>
-                        <a href="#info"><IconServer/> Server Info</a>
-                        <a href="#code"><IconCode/> Code</a>
-                        <a href="#update"><IconSync/> Update</a>
+                        <a href="#general" data-checked={hash == '#general'}><IconSettings/> General</a>
+                        <a href="#meta" data-checked={hash == '#meta'}><IconNote/> Meta</a>
+                        <a href="#data" data-checked={hash == '#data'}><IconDatabase/> Data</a>
+                        <a href="#advanced" data-checked={hash == '#advanced'}><IconTerminal/> Advanced</a>
+                        <a href="#info" data-checked={hash == '#info'}><IconServer/> Server Info</a>
+                        <a href="#code" data-checked={hash == '#code'}><IconCode/> Code</a>
+                        <a href="#update" data-checked={hash == '#update'}><IconSync/> Update</a>
                     </div>
                     <p class="version">Version: {version}</p>
                 </div>
             </div>
+            {settings && <>
+                {hash == '#general' && <General data={data} setData={setData}/>}
+            </>}
         </div>
         <div id="image-dialog" class="dialog image-dialog">
             <div></div>
