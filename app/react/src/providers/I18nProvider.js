a82adc7da12276a9632506a91e6191a1f9d98a00
Usbac <castillo.alex97@outlook.com>
2025-12-28 19:50:53 +0100

    Add I18N provider

---
+++ app/react/src/providers/I18nProvider.js
@@ -0,0 +1,56 @@
+import React, { createContext, useContext, useState, useEffect, useMemo } from 'react';
+
+const I18nContext = createContext();
+
+export const I18nProvider = ({ children, defaultLanguage = 'en' }) => {
+    const [ language, setLanguage ] = useState(localStorage.getItem('lang') || defaultLanguage);
+    const translations = useMemo(() => {
+        const translation_context = require.context('../lang', false, /\.js$/);
+        const res = {};
+        
+        translation_context.keys().forEach((file_name) => {
+            res[file_name.replace('./', '').replace('.js', '')] = translation_context(file_name).default;
+        });
+        
+        return res;
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('lang', language);
+    }, [ language ]);
+
+    const t = (key, ...params) => {
+        const translation = translations[language]?.[key];
+
+        if (!translation) {
+            throw new Error(`Unknown translation key: "${key}" for language "${language}"`);
+        }
+
+        let i = 0;
+        return translation.replace(/%s|%d|%f/g, e => params[i++] ?? e);
+    };
+
+    const changeLanguage = (lang) => {
+        if (translations[lang]) {
+            setLanguage(lang);
+        }
+    };
+
+    return <I18nContext.Provider value={{
+        language: language,
+        t: t,
+        changeLanguage: changeLanguage,
+    }}>
+        {children}
+    </I18nContext.Provider>;
+};
+
+export const useI18n = () => {
+    const context = useContext(I18nContext);
+
+    if (!context) {
+        throw new Error('useI18n must be used within an I18nProvider');
+    }
+
+    return context;
+};
