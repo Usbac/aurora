fa63aa8add9f21a18c1aeed51a9a10ea3896028c
Usbac <castillo.alex97@outlook.com>
2025-12-29 23:42:02 +0100

    Add i18n context

--- app/react/src/index.js
+++ app/react/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { I18nProvider } from './providers/I18nProvider';
 import AdminPages from './components/AdminPages';
 import NewPassword from './pages/NewPassword';
 import Login from './pages/Login';
@@ -23,28 +24,30 @@ const App = () => {
     const query_client = new QueryClient();
 
     return <BrowserRouter>
-        <QueryClientProvider className="app" client={query_client}>
-            <Routes>
-                <Route path="/admin/new_password" element={<NewPassword/>}/>
-                <Route path="/admin" element={<Login/>}/>
-                <Route path="/admin" element={<AdminPages/>}>
-                    <Route path="dashboard" element={<Dashboard/>}/>
-                    <Route path="pages" element={<Pages/>}/>
-                    <Route path="posts" element={<Posts/>}/>
-                    <Route path="users" element={<Users/>}/>
-                    <Route path="media" element={<Media/>}/>
-                    <Route path="links" element={<Links/>}/>
-                    <Route path="tags" element={<Tags/>}/>
-                    <Route path="settings" element={<Settings/>}/>
-                    <Route path="pages/edit" element={<Page/>}/>
-                    <Route path="posts/edit" element={<Post/>}/>
-                    <Route path="links/edit" element={<Link/>}/>
-                    <Route path="tags/edit" element={<Tag/>}/>
-                    <Route path="users/edit" element={<User/>}/>
-                </Route>
-                <Route path="*" element={<div>404 Not Found</div>}/>
-            </Routes>
-        </QueryClientProvider>
+        <I18nProvider defaultLanguage="en">
+            <QueryClientProvider className="app" client={query_client}>
+                <Routes>
+                    <Route path="/admin/new_password" element={<NewPassword/>}/>
+                    <Route path="/admin" element={<Login/>}/>
+                    <Route path="/admin" element={<AdminPages/>}>
+                        <Route path="dashboard" element={<Dashboard/>}/>
+                        <Route path="pages" element={<Pages/>}/>
+                        <Route path="posts" element={<Posts/>}/>
+                        <Route path="users" element={<Users/>}/>
+                        <Route path="media" element={<Media/>}/>
+                        <Route path="links" element={<Links/>}/>
+                        <Route path="tags" element={<Tags/>}/>
+                        <Route path="settings" element={<Settings/>}/>
+                        <Route path="pages/edit" element={<Page/>}/>
+                        <Route path="posts/edit" element={<Post/>}/>
+                        <Route path="links/edit" element={<Link/>}/>
+                        <Route path="tags/edit" element={<Tag/>}/>
+                        <Route path="users/edit" element={<User/>}/>
+                    </Route>
+                    <Route path="*" element={<div>404 Not Found</div>}/>
+                </Routes>
+            </QueryClientProvider>
+        </I18nProvider>
     </BrowserRouter>;
 };
 
