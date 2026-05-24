5061f04d21734bc5d62b62246fdde5ed6606a57c
Usbac <castillo.alex97@outlook.com>
2026-05-24 16:36:52 +0200

    Remove unused import

--- app/react/src/pages/User.js
+++ app/react/src/pages/User.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getContentUrl, getUrl, ImageDialog, Input, LoadingPage, MenuButton, Switch, Textarea, useApi, useRequest, formatDate, getRoleTitle, getSlug, getDeviceInfo, getDeviceType, Dropdown } from '../utils/utils';
-import { IconDatabase, IconDesktop, IconEye, IconInfo, IconKey, IconMobile, IconTrash, IconUsers } from '../utils/icons';
+import { IconDesktop, IconEye, IconInfo, IconKey, IconMobile, IconTrash, IconUsers } from '../utils/icons';
 import { useLocation, useNavigate, useOutletContext } from 'react-router-dom';
 import { useI18n } from '../providers/I18nProvider';
 
